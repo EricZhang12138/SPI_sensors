cc43ba5c1fa8d6bbc73066a289211081d0b38a57
Yuou Zhang <YZhang@metrol.co.uk>
2025-07-17 15:11:07 +0100

    Added a seperate thread which prints sensor readings every 6 seconds to the terminal

--- log_spi.py
+++ log_spi.py
@@ -1,16 +1,39 @@
 import serial
 import datetime
+import threading
+import time
+
+
+def print_to_console():
+    while True:
+        time.sleep(6)
+        try:
+            lock.acquire()
+            print(buffer_data)
+            buffer_data.clear()
+        finally:
+            lock.release()
+
 
 SERIAL_PORT = 'COM6'
 BAUD_RATE = 9600
 counter =0
 pattern = [b'\r',b'\n',b'\r',b'\n']
+buffer_data = bytearray()
+lock = threading.Lock()
+
+
+
 
 # create a unique filename based on the current date and time
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 log_filename = f"data_log\spi_log_{timestamp}.txt"
 
 print(f"Connecting to port {SERIAL_PORT} at {BAUD_RATE} bps...")
+
+thread_print = threading.Thread(target = print_to_console,daemon=True)
+thread_print.start()
+
 
 try:
     with serial.Serial(SERIAL_PORT,BAUD_RATE,timeout=3) as ser:
@@ -31,11 +54,23 @@ try:
                     current_instant = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
                     timestamp_message = f"Time: ({current_instant})\r\n".encode('utf-8')
                     file.write(timestamp_message)
+                    try:
+                        lock.acquire()
+                        buffer_data.extend(timestamp_message)
+                    finally:
+                        lock.release()
+
                     counter =0
 
                 if incoming_byte:
                     #write the raw byte directly to the file
                     file.write(incoming_byte)
+                    try:
+                        lock.acquire()
+                        buffer_data.extend(incoming_byte)
+                    finally:
+                        lock.release()
+                
                 else:
                     print("data unavailable")
 
