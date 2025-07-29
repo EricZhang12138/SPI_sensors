f73ece70572a66d920c680dbccf862be73318e5f
EricZhang12138 <2067716083zyo@gmail.com>
2025-07-29 15:36:22 +0100

    now it prints to a csv file, not a text file

--- log_spi.py
+++ log_spi.py
@@ -2,14 +2,14 @@ import serial
 import datetime
 import threading
 import time
+import csv
 
 def print_to_console():
     global count
     while True:
-        	
         try:
             lock.acquire()
-            print(buffer_data.decode('utf-8'))
+            print(buffer_data.decode('utf-8',errors='ignore'))
             buffer_data.clear()
         finally:
             lock.release()
@@ -19,32 +19,34 @@ SERIAL_PORT = '/dev/ttyACM0'
 BAUD_RATE = 9600
 counter =0
 pattern = [b'\r',b'\n',b'\r',b'\n']
-buffer_data = bytearray(54)
+buffer_data = bytearray(64)
 lock = threading.Lock()
+buffer_str = ""
 
 
 # create a unique filename based on the current date and time
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_filename = f"data_log/spi_log_{timestamp}.txt"
+log_filename = f"data_log/spi_log_{timestamp}.csv"
 
 print(f"Connecting to port {SERIAL_PORT} at {BAUD_RATE} bps...")
 
-time.sleep(1)
-try:
-    lock.acquire()
-    buffer_data.clear()
-finally:
-    lock.release()
+
 
 thread_print = threading.Thread(target = print_to_console,daemon=True)
 thread_print.start()
 
 try:
     with serial.Serial(SERIAL_PORT,BAUD_RATE,timeout=3) as ser:
-        print(f"Connected. Logging data to '{log_filename}'. Press Ctrl+C to stop.")
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+        time.sleep(0.1)  # Give it a moment
 
+        print(f"Connected. Logging data to '{log_filename}'. Press Ctrl+C to stop.")
+        buffer_data.clear()
         # open the log file for writing
-        with open(log_filename,'wb') as file:
+        with open(log_filename,'w',newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Timestamp','tmp126 reading','lm95071 reading'])
             while True:
                 #read any available data from the serial port
                 incoming_byte = ser.read()
@@ -57,27 +59,29 @@ try:
                 if counter ==4:
                     current_instant = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
                     timestamp_message = f"Time: ({current_instant})\n".encode('utf-8')
-                    file.write(timestamp_message)
+                    timestamp_str = f"{current_instant}"
                     try:
                         lock.acquire()
                         buffer_data.extend(timestamp_message)
                     finally:
                         lock.release()
-
                     counter =0
-
+                    buffer_str = buffer_data.decode('utf-8')
+                    tmp126_read = buffer_str[8:13]
+                    lm9521_read = buffer_str[25:30]
+                    writer.writerow([timestamp_str,tmp126_read,lm9521_read])
+                    
                 if incoming_byte:
                     #write the raw byte directly to the file
                     #covert the windows \r\n to linux \n
                     if incoming_byte == b'\r':
                         continue
-                    file.write(incoming_byte)
                     try:
                         lock.acquire()
                         buffer_data.extend(incoming_byte)
                     finally:
                         lock.release()
-                
+
                 else:
                     print("data unavailable")
 
